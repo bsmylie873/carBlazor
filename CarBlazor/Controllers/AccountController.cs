39340ba0980f884dc334276a1ca2f757f4ac6c78
Brandon Smylie <brandon.smylie@unosquare.com>
2025-04-14 16:22:43 +0100

    Small adjustment.

--- CarBlazor/Controllers/AccountController.cs
+++ CarBlazor/Controllers/AccountController.cs
@@ -4,31 +4,30 @@ using Microsoft.AspNetCore.Mvc;
 using CarBlazor.Services;
 using CarBlazor.ViewModels;
 
-namespace CarBlazor.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AccountController : ControllerBase
-    {
-        private readonly AuthService _authService;
+namespace CarBlazor.Controllers;
 
-        public AccountController(AuthService authService)
-        {
-            _authService = authService;
-        }
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly AuthService _authService;
+
+    public AccountController(AuthService authService)
+    {
+        _authService = authService;
+    }
         
-        [HttpPost("login")]
-        public async Task<IActionResult> Login([FromForm] LoginViewModel model)
-        {
-            var (success, passwordResetRequired, userId) = await _authService.LoginAsync(model.Username, model.Password, model.RememberMe);
-            return !success ? Redirect("/login?error=true") : Redirect(passwordResetRequired ? $"/change-password/{userId}?reset=true" : "/");
-        }
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromForm] LoginViewModel model)
+    {
+        var (success, passwordResetRequired, userId) = await _authService.LoginAsync(model.Username, model.Password, model.RememberMe);
+        return !success ? Redirect("/login?error=true") : Redirect(passwordResetRequired ? $"/change-password/{userId}?reset=true" : "/");
+    }
         
-        [HttpPost("logout")]
-        public async Task<IActionResult> Logout()
-        {
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return Redirect("/login");
-        }
+    [HttpPost("logout")]
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return Redirect("/login");
     }
 }
